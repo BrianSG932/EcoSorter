65586e4f270ed7bc5357edcc8a0ab16941d6fdd1
Fernandocruztovar332 <gtct2218@gmail.com>
2025-04-08 05:17:44 -0600

    correccion error main en la api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from app.routes import clasificador
+from app.routes import residuos  # o el nombre real de tu archivo en /routes/
 
 app = FastAPI(title="EcoSorter API")
 
-app.include_router(clasificador.router)
+app.include_router(residuos.router)
